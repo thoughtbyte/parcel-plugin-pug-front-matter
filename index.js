ea5fbe8e0dd7a9877569d935a144bb32985da24b
Vincent Abruzzo <vgabruzzo@gmail.com>
2018-06-11 14:11:33 -0400

    bump version, fix index

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 module.exports = function(bundler) {
-  bundler.addAssetType('pug', require.resolve('./src/PugAsset.js'));
-  bundler.addAssetType('jade', require.resolve('./src/PugAsset.js'));
+  bundler.addAssetType('pug', require.resolve('./PugAsset.js'));
+  bundler.addAssetType('jade', require.resolve('./PugAsset.js'));
 };
